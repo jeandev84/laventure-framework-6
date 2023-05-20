d9e56d861e8ff0a491bb23bf459634c1137a5eac
Жан-Клод <jeanyao@ymail.com>
2023-05-21 01:37:52 +0300

    fix bug

--- src/Component/Routing/Route/Mix.php
+++ src/Component/Routing/Route/Mix.php
@@ -3,15 +3,6 @@ namespace Laventure\Component\Routing\Route;
 
 class Mix
 {
-    /**
-     * @var array
-    */
     public static $namedRoutes = [];
-
-
-
-    /**
-     * @var array
-    */
     public static $middlewares = [];
 }
